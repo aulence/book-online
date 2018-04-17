8edd5a96a070c450cfd6f966981e30761accab09
Huangbr <995257989@qq.com>
2018-04-17 11:31:22 +0800

    无法运行的错误

--- src/page/home/HomePage.vue
+++ src/page/home/HomePage.vue
@@ -1,11 +1,11 @@
 <template>
  <div>
-    
+    123
  </div>
 </template>
 
 <script src="./home-page.js"></script>
 
 <style lang='scss' scoped>
-    @import './home-page.scss';
+    // @import './home-page.scss';
 </style>
